1fc416f583d3ba956b92585f30d4d563368482e2
Kimjungyu-04 <kjg3914@gmail.com>
2024-09-29 14:35:29 +0900

    장비 장착 ui 프리펩
    만듬

---
+++ Chobo_UnityProject/Assets/Script/Equipment/EquipmentUIManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EquipmentUIManager : MonoBehaviour
+{
+    public GameObject equipmentPanel;  // 장비 목록 창 (Panel)
+
+    // 장비 선택 버튼 클릭 시 호출되는 함수
+    public void OnEquipmentButtonClicked()
+    {
+        // 장비 목록 창의 활성화 여부를 토글
+        equipmentPanel.SetActive(!equipmentPanel.activeSelf);
+    }
+}
