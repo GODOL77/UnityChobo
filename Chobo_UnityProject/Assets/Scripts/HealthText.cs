20dbc1887e5d62a0f1bf3bab2c64f3b64ec319ff
GODOL <sihyun9346@mokwon.ac.kr>
2024-10-07 17:29:12 +0900

    이승현 UI스크립트 추가

--- Chobo_UnityProject/Assets/Scripts/HealthText.cs
+++ Chobo_UnityProject/Assets/Scripts/HealthText.cs
@@ -6,11 +6,26 @@ using UnityEngine;
 public class HealthText : MonoBehaviour
 {
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI MindText;
+    public TextMeshProUGUI ATKText;
+    public TextMeshProUGUI DEFText;
+    public TextMeshProUGUI SPEEDText;
+    public TextMeshProUGUI CCText;
+    public TextMeshProUGUI AVText;
+    public TextMeshProUGUI HITText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        healthText.text = "100";
+        healthText.text = "10";
+        MindText.text = "10";
+        ATKText.text = "10";
+        DEFText.text = "10";
+        SPEEDText.text = "10";
+        CCText.text = "10";
+        AVText.text = "10";
+        HITText.text = "10";
+
     }
 
-   
 }
