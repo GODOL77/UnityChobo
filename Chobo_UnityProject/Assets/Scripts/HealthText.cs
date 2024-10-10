42f51f38ea98c5a018d74ea2a44009c033087aa7
0Raim0 <dltmdgus446@naver.com>
2024-10-10 23:46:08 +0900

    스테이터스 증가 테스트 버튼 추가

--- Chobo_UnityProject/Assets/Scripts/HealthText.cs
+++ Chobo_UnityProject/Assets/Scripts/HealthText.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthText : MonoBehaviour
 {
@@ -13,11 +14,15 @@ public class HealthText : MonoBehaviour
     public TextMeshProUGUI CCText;
     public TextMeshProUGUI AVText;
     public TextMeshProUGUI HITText;
-    
+    public Button TestButton;
+
+    private int health; // health 값을 저장할 변수
+
     // Start is called before the first frame update
     void Start()
     {
-        healthText.text = "10";
+        health = 10; // 초기화된 health 값 설정
+        healthText.text = health.ToString();
         MindText.text = "10";
         ATKText.text = "10";
         DEFText.text = "10";
@@ -25,7 +30,15 @@ public class HealthText : MonoBehaviour
         CCText.text = "10";
         AVText.text = "10";
         HITText.text = "10";
-
     }
 
+    void Update()
+    {
+        // 마우스 왼쪽 버튼이 클릭되었을 때
+        if (Input.GetMouseButtonDown(0))
+        {
+            health += 10; // health 값을 10 증가
+            healthText.text = health.ToString(); // 증가된 값을 텍스트로 변환하여 표시
+        }
+    }
 }
