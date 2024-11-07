7517e155386804a23b99e4a9524eaf0fa8f84a40
0Raim0 <dltmdgus446@naver.com>
2024-11-07 20:05:28 +0900

    c키로 인벤토리 열기 재추가

--- Chobo_UnityProject/Assets/Scripts/CharacterPanelController.cs
+++ Chobo_UnityProject/Assets/Scripts/CharacterPanelController.cs
@@ -17,4 +17,19 @@ public class CharacterPanelController : MonoBehaviour {
         CharacterPanels.SetActive(false);
         Debug.Log("ㄴㄴㄴㄴ");
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (CharacterPanels.activeSelf)
+            {
+                HideCharacterPanels();
+            }
+            else
+            {
+                ShowCharacterPanels();
+            }
+        }
+    }
 }
